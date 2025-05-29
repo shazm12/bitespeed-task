9410b99dee8e269972b8cce162d3885c9c2c116b
shazm12 <shamik.bera2019@vitstudent.ac.in>
2025-05-29 23:51:17 +0530

    chore: remove unused code;

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,12 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-
 import contactRouter from "./routes/contact";
 dotenv.config();
 
 const app: Express = express();
 app.use(express.json());
 app.use(cors());
-const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
